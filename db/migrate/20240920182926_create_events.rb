c3f59630a953ef8402ac884ebc629350ad111be5
Maria Virginia Espeche <virginia.espechekaram@gmail.com>
2024-09-20 15:47:43 -0300

    database creation and migrations

---
+++ db/migrate/20240920182926_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[7.2]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.datetime :start_time
+      t.datetime :end_time
+      t.string :location
+      t.string :weather_info
+      t.references :worker, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
